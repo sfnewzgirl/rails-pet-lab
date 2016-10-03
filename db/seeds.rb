a702ccda090bf06c59fd7d393a7ea2ed2a614312
Brianna Veenstra <b.g.veenstra@gmail.com>
2016-10-03 16:17:54 -0700

    uncomment pet seeds in solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,23 +24,23 @@ end
 owners = Owner.create(owners_data)
 
 # Pets
-#
-# def random_pet_breed
-#   ["dog", "cat", "reptile", "rabbit", "rodent", "rock", "amphibian", "giant robot", "fish"].sample
-# end
-#
-# def random_recent_date(min_days_ago, max_days_ago)
-#   rng = Random.new
-#   (DateTime.now - rng.rand(min_days_ago..max_days_ago)).to_date
-# end
-#
-#
-# 12.times do
-#   pet_data = {
-#     name: FFaker::Name.first_name,
-#     breed: random_pet_breed,
-#     # date_of_birth: random_recent_date(3.0, 2000.0)
-#   }
-#   pet = Pet.create(pet_data)
-#   owners.sample.pets << pet
-# end
+
+def random_pet_breed
+  ["dog", "cat", "reptile", "rabbit", "rodent", "rock", "amphibian", "giant robot", "fish"].sample
+end
+
+def random_recent_date(min_days_ago, max_days_ago)
+  rng = Random.new
+  (DateTime.now - rng.rand(min_days_ago..max_days_ago)).to_date
+end
+
+
+12.times do
+  pet_data = {
+    name: FFaker::Name.first_name,
+    breed: random_pet_breed,
+    # date_of_birth: random_recent_date(3.0, 2000.0)
+  }
+  pet = Pet.create(pet_data)
+  owners.sample.pets << pet
+end
