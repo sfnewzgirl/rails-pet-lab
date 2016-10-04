5d1bcfa62ea7849fc8298e2ea51360a47d1a11a6
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-03 20:49:05 -0700

    adds validation for owner, error

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -9,8 +9,13 @@ class OwnersController < ApplicationController
   end
 
   def create
-    owner = Owner.create(owner_params)
-    redirect_to owner_path(owner)
+    owner = Owner.new(owner_params)
+    if owner.save
+      redirect_to owner_path(owner)
+    else
+      flash[:error] = owner.errors.full_messages.join(", ")
+    redirect_to new_owner_path
+    end
   end
 
   def show
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,8 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
+  validates :first_name, presence: true, length: {maximum: 255}
+  validates :last_name, presence: true, length: {maximum: 255}
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: { case_sensitive: true}, format: { with: /\A[@]+\z/,
+    message: "is not a valid address" }
 
   before_save :normalize_phone_number
 
